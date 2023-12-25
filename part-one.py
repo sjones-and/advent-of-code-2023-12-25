e2e3e1f57a607d9e24bf116337213bfe15c355fa
Stuart Jones <47328516+sjones-and@users.noreply.github.com>
2023-12-25 12:12:35 +0000

    Initial commit

---
+++ part-one.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import os
+from time import perf_counter_ns
+
+def answer(input_file):
+    start = perf_counter_ns()
+    with open(input_file, 'r') as input:
+        data = input.read().split('\n')
+
+    answer = None
+    end = perf_counter_ns()
+
+    print(f'The answer is: {answer}')
+    print(f'{((end-start)/1000000):.2f} milliseconds')
+
+input_file = os.path.join(os.path.dirname(__file__), 'input')
+answer(input_file)
